0e07944a22ffba4eb6906d1e1d3261088c85e452
viowla <55015538+viowla@users.noreply.github.com>
2020-10-01 18:42:20 +0600

    add bus images, list view

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,11 +1,26 @@
 package com.example.test;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String title[]={"City 1-City 2","City 1-City 2","City 1-City 2","City 1-City 2"};
+    String subtitle[]={"XX.XX.XXXX XX:XX","XX.XX.XXXX XX:XX","XX.XX.XXXX XX:XX","XX.XX.XXXX XX:XX"};
+    int images[]={R.drawable.bus1,R.drawable.bus2,R.drawable.bus3,R.drawable.bus4};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +29,43 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        listView=(ListView) findViewById(R.id.listView);
+
+        MyAdapter myAdapter=new MyAdapter(this,title,subtitle,images);
+        listView.setAdapter(myAdapter);
     }
+
+    class MyAdapter extends ArrayAdapter<String> {
+        Context context;
+        String title[];
+        String subtitle[];
+        int img[];
+
+        public MyAdapter(Context c, String[] title, String[] subtitle, int[] img){
+            super(c, R.layout.row,R.id.textView1, title);
+            this.context=c;
+            this.title=title;
+            this.subtitle=subtitle;
+            this.img=img;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row,parent,false);
+            ImageView imageView=row.findViewById(R.id.image);
+            TextView textView1 = row.findViewById(R.id.textView1);
+            TextView textView2 = row.findViewById(R.id.textView2);
+
+
+            imageView.setImageResource(images[position]);
+            textView1.setText(title[position]);
+            textView2.setText(subtitle[position]);
+
+            return row;
+        }
+    }
+
 }
